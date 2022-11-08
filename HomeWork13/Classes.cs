99410497c0dbc995c89676fb67c148d8e3fca168
DmEnd <Dima_polit-sov@mail.ru>
2022-11-09 02:00:04 +0300

    Добавьте файлы проекта.

---
+++ HomeWork13/Classes.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork13
+{
+    internal class Building
+    {
+        public string Adress { get; set; }
+        public double Length { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Building(string adress, double length, double width, double height)
+        {
+            Adress = adress;
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public string Print()
+        {
+            return $"{Adress} {Length} {Width} {Height}";
+        }
+
+    }
+
+    sealed class MultiBuilding : Building
+    {
+        public int Floors { get; set; }
+
+        public MultiBuilding(string adress, double length, double width, double height, int floors)
+            : base(adress, length, width, height)
+        {
+            Floors = floors;
+        }
+
+        public string Print()
+        {
+            return $"{base.Print()} {Floors}";
+        }
+    }
+}
+
---
+++ HomeWork13/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Building bulding = new Building("ул.Садовая", 20, 10.5, 9);
+            Console.WriteLine(bulding.Print());
+
+            MultiBuilding multiBuilding = new MultiBuilding("ул.Арбузная", 40, 20.6, 15,5);
+            Console.WriteLine(multiBuilding.Print());
+
+            Console.ReadKey();
+        }
+    }
+}
